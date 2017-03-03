e5ddf4ae3f625e2ffc95fb4ae3bc15cacac9868b
kathrycg <kathrycg@uci.edu>
2017-03-02 16:01:02 -0800

    Add simple start menu scene & script

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour {
+
+	public void loadGame()
+	{
+		SceneManager.LoadScene (1);
+	}
+}
